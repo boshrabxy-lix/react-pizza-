d93d4289a1e5f6c66c74decfd613797ec7fc32e9
boshrabxy-lix <boshrabxy@gmail.com>
2026-06-24 01:56:37 +0300

    potato page

--- src/assets/components/moreRecipes/PotatoMenu.jsx
+++ src/assets/components/moreRecipes/PotatoMenu.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
-
+import useFetch from "../hooks/UseFetch";
+import Loader from "../loader/Loader";
+import Post from "../posts/Post";
 
 export default function PotatoMenu() {
+ const { data, isLoader, error } = useFetch(
+    "http://forkify-api.jonas.io/api/v2/recipes?search=potato",
+  );
+  if (isLoader) return <Loader />;
+  if (error) return <div className="alert alert-danger">{error}</div>;
+
   return (
-    <div>PotatoMenu</div>
+   <section className="posts mb-3 py-2">
+         <div className="container">
+           <h2 className="text-center mt-3 mp-2 py-3" fontSize={"35px"}>
+             Potato Recipes Menu
+           </h2>
+           <div className="row gap-4">
+             {data.map((post) => (
+               <Post post={post} key={post.id} />
+             ))}
+           </div>
+         </div>
+       </section>
   )
 }
